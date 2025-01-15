e90edbfbb5241206cb4bd7d381a995887292e238
Virgiawan <mainzz1945@gmail.com>
2025-01-15 14:42:39 +0700

    susulan tugas 5

--- Tugas/tugas5.php
+++ Tugas/tugas5.php
@@ -4,5 +4,33 @@ Buatlah program berdasarkan materi yang sudah diberikan, minimal menggunakan 2 k
 -->
 
 <?php
-   //
+   for ($hari = 1;$hari<=7; $hari++){
+      echo "Hari ke- $hari adalah-";
+  
+  switch ($hari){
+      case 1:
+          echo "Senin<br>";
+          break;
+      case 2;
+           echo "Selasa<br>";
+           break;
+           case 3;
+           echo "Rabu<br>";
+           break;
+           case 4;
+           echo "Kamis<br>";
+           break;
+           case 5;
+           echo "Jum'at<br>";
+           break;
+           case 6;
+           echo "Sabtu<br>";
+           break;
+           case 7;
+           echo "Minggu<br>";
+           break;
+           default;
+           echo "Tidak valid <br>";
+          }
+      }
 ?>
