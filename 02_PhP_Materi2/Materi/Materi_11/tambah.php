3e5cde9950589b991475e5d3d640928ea5102385
Virgiawan <mainzz1945@gmail.com>
2025-02-06 10:25:18 +0700

    ingyh

--- 02_PhP_Materi2/Materi/Materi_11/tambah.php
+++ 02_PhP_Materi2/Materi/Materi_11/tambah.php
@@ -1,67 +1,73 @@
 <?php 
-
-
 require 'functions.php';
+// cek apakah tombol submit sudah di tekan atau belum
+if( isset($_POST["submit"]) ) {
 
-
-if ( isset ($_POST["Submit"] ) ) {
-
-    if ( tambah ($_POST) > 0 ) {
-        echo "
-               <script>
-                alert('Data berhasil Diinput') 
-                document.location.href = 'index.php'
-               </script>";
-    } else {
+    // cek apakah data berhasil ditambahkan atau tidak
+    if( tambah($_POST) > 0 ) {
         echo "
         <script>
-        alert('Data tidak berhasil diinput')
+        alert('data berhasil ditambahkan');
         document.location.href = 'index.php'
-        </script>";
+        </script>
+        ";
+    } else {
+        echo "
+         <script>
+        alert('data gagal ditambahkan');
+        document.location.href = 'index.php'
+        </script>
+        ";
     }
-
 }
+    
+
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambahan data </title>
+    <title>Tambah Data Mahasiswa </title>
 </head>
 <body>
-    <h1>Tambahan Data Kendaraan</h1>
+    <h1>Tambah Data Mahasiswa</h1>
 
     <form action="" method="post">
-        <ul>
-            
-            <li>
-                <label for="Varian">Varian:</label>
-                <input type="text" name="Varian" id="Varian">
-            </li>
-            <li>
-            <label for="Persenjataan">Persenjataan:</label>
-            <input type="text" name="Persenjataan" id="Persenjataan">
-            </li>
-            <li>
-            <label for="Amunisi">Tipe Amunisi:</label>
-            <input type="text" name="Amunisi" id="Amunisi">
-            </li>
-            <li>
+
+    <ul>
+        <li>
+            <label for="Varian">Varian:</label>
+            <input type="text" name="Varian" id="Varian"
+            required>
+        </li>
+        <li>
+            <label for="Armaments">Armaments:</label>
+            <input type="text" name="Armaments" id="Armaments"
+            required>
+        </li>
+        <li>
+            <label for="Amunisi">Amunisi:</label>
+            <input type="text" name="Amunisi" id="Amunisi"
+            required>
+        </li>
+        <li>
             <label for="Faksi">Faksi:</label>
-            <input type="text" name="Faksi" id="Faksi">
-            </li>
-            <li>
+            <input type="text" name="Faksi" id="Faksi"
+            required>
+        </li>
+        <li>
             <label for="Gambar">Gambar:</label>
-            <input type="text" name="Gambar" id="Gambar">
-            </li>
-            <li>
-                <button type= "Submit" name="Submit">Tambahkan Kendaraan!</button>
-            </li>
-        </ul>
-    
+            <input type="text" name="Gambar" id="Gambar"
+            required>
+        </li>
+        <br>
+        <li>
+            <button typr="submit" name="submit">Tambah data</button>
+        </li>
+    </ul>
     </form>
-
-
 </body>
 </html>
