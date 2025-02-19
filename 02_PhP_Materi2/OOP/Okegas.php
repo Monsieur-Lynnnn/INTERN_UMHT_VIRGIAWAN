9c99b705f5edf489369e4abd4228b39883337aac
Virgiawan <mainzz1945@gmail.com>
2025-02-19 11:10:32 +0700

    Setter

--- 02_PhP_Materi2/OOP/Okegas.php
+++ 02_PhP_Materi2/OOP/Okegas.php
@@ -1,46 +1,76 @@
 <?php
 class Produk {
-    private $judul, $penulis, $penerbit; 
-    Protected $diskon = 0; 
-    Private $harga;
+    private $judul, $penulis, $penerbit, $harga, $diskon = 0; 
     
     public function __construct($judul = "judul", $penulis = "penulis", 
-    $penerbit = "penerbit", $harga = 0 ) {
+        $penerbit = "penerbit", $harga = 0 ) {
         $this->judul = $judul;
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
         $this->harga = $harga;
     }
     
-    public function setDiskon( $diskon ) {
+    public function setDiskon($diskon) {
         $this->diskon = $diskon;
+    }
+     
+    public function getDiskon() {
+        return $this->diskon;
+    }
+
+    public function setJudul( $judul ) { 
+        if( !is_string($judul) ) {
+            throw new Exception("Judul Harus String");
+        }
+        $this->judul = $judul;
+    }
+
+    public function getJudul() {
+        return $this->judul;
+    }
+
+    public function setHarga( $harga ) {
+        $this->harga = $harga;
     }
 
     public function getHarga() {
-        return $this->harga - ( $this->harga * $this->diskon / 100 );
+        return $this->harga - ($this->harga * $this->diskon / 100);
     }
-                             
+    
     public function getLabel() {
         return "$this->penulis, $this->penerbit";
     }
-
+    
+    public function setPenulis( $penulis ) {
+        $this->penulis = $penulis;
+    }
+    
+    public function getPenulis() {
+        return $this->penulis;
+    }
+    
+    public function setPenerbit( $penerbit ) {
+        $this->penerbit = $penerbit;
+    }
+    
+    public function getPenerbit() {
+        return $this->penerbit;
+    }
+    
     public function getIngfoProduk() {
-        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+        $str = "{$this->getJudul()} | {$this->getLabel()} (Rp. {$this->getHarga()})";
         return $str;
     }
 }
 
 class Komik extends Produk {
     public $jmlhalaman;
-    public function __construct($judul = "judul", $penulis = "penulis", 
-    $penerbit = "penerbit", $harga = 0, $jmlhalaman = 0){
     
-   parent::__construct($judul, $penulis, 
-   $penerbit, $harga );
-
-   $this->jmlhalaman = $jmlhalaman;
-    
+    public function __construct($judul, $penulis, $penerbit, $harga, $jmlhalaman) {
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+        $this->jmlhalaman = $jmlhalaman;
     }
+    
     public function getIngfoProduk() {
         $str = "Komik : " . parent::getIngfoProduk() . " - {$this->jmlhalaman} Halaman.";
         return $str; 
@@ -49,13 +79,11 @@ class Komik extends Produk {
 
 class Game extends Produk {
     public $waktumain;
-    public function __construct($judul = "judul", $penulis = "penulis", 
-    $penerbit = "penerbit", $harga = 0, $waktumain = 0){
-        parent::__construct($judul, $penulis, 
-        $penerbit, $harga );
+    
+    public function __construct($judul, $penulis, $penerbit, $harga, $waktumain) {
+        parent::__construct($judul, $penulis, $penerbit, $harga);
         $this->waktumain = $waktumain;
     }
-    
     
     public function getIngfoProduk() {
         $str = "Game : " . parent::getIngfoProduk() . " ~ {$this->waktumain} Jam.";
@@ -65,22 +93,24 @@ class Game extends Produk {
 
 class CetakIngfoProduk {
     public function cetak(Produk $produk) {
-        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        $str = "{$produk->getJudul()} | {$produk->getLabel()} (Rp. {$produk->getHarga()})";
         return $str;
     }
 }
 
-
+// Membuat objek
 $produk3 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
 $produk4 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 450000, 50);
-
 
 echo $produk3->getIngfoProduk();
 echo "<br>";
 echo $produk4->getIngfoProduk();
 echo "<hr>";
+
+// Menerapkan diskon pada game
 $produk4->setDiskon(60);
-echo $produk4->getHarga();
+echo "Harga setelah diskon: Rp. " . $produk4->getHarga();
 echo "<hr>";
-$produk5 = new Produk("Barangbaru");
-echo $produk5->judul;
+
+$produk3->setPenulis("Fadhil ganteng sikma skibidi");
+echo $produk3->getPenulis();
